eb6d5ca3d5a501f6773781b73ced05775f8a35fe
Jose <josemanuelrusmanjon@gmail.com>
2023-11-20 19:47:39 +0100

    fixed threads

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     private static Logger log;
     private static LectorDatos data;
-    static ExecutorService executor = Executors.newCachedThreadPool();
+    static ExecutorService executor = Executors.newFixedThreadPool(4);
 
 
     public static void runFiles() throws IOException, InterruptedException {
