e4d0205884d185e4229cc2a45843ff4e1b0e40cf
Jose <josemanuelrusmanjon@gmail.com>
2023-11-23 16:16:47 +0100

    created files for DiffEvolution

--- src/ALGGenOX2.java
+++ src/ALGGenOX2.java
@@ -3,7 +3,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class GenOX2 {
+public class ALGGenOX2 {
     private final int numElites;
     private final int numKBest;
     private int generation = 0;
@@ -15,7 +15,7 @@ public class GenOX2 {
     private ArrayList<Individual> newPopulation = new ArrayList<>();
     private int[][] cities;
 
-    public GenOX2(long seed, int elite, int kbest, Logger log, int[][] citiesList) {
+    public ALGGenOX2(long seed, int elite, int kbest, Logger log, int[][] citiesList) {
         rand = new Random(seed);
         numElites = elite;
         numKBest = kbest;
--- src/Metaheuristic.java
+++ src/Metaheuristic.java
@@ -29,7 +29,7 @@ public class Metaheuristic implements Runnable {
     public void createLogger(Long seed, int elite, int kBest, int kWorst, int population) throws IOException {
         String logFile = "log/" + Utils.config.getFile() + "_" + seed + "_P=" + population + "_E=" + elite + "_kBest=" + kBest + "_kWorst=" + kWorst + ".txt";
 
-        log = Logger.getLogger(GenOX2.class.getName() + " " + logFile);
+        log = Logger.getLogger(ALGGenOX2.class.getName() + " " + logFile);
         if (Utils.config.getConsoleLog()) {
             ConsoleHandler consoleHand = new ConsoleHandler();
             log.addHandler(consoleHand);
@@ -50,7 +50,7 @@ public class Metaheuristic implements Runnable {
             throw new RuntimeException(e);
         }
         Integer actualEvaluations = 0;
-        GenOX2 genes = new GenOX2(seed, elite, kBest, log, cities);
+        ALGGenOX2 genes = new ALGGenOX2(seed, elite, kBest, log, cities);
 
         genes.initialization(population, data.getCiudades().length);
         long initTime = System.currentTimeMillis();
